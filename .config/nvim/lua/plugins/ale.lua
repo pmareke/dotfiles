13d97635ab1fff22b54f8510ac879f1563739d71
Pedro Lopez Mareque <pedro.lopez.mareque@gmail.com>
2022-11-22 21:23:29 +0100

    feat: remove ALE files

--- .config/nvim/lua/plugins/ale.lua
+++
@@ -1,9 +0,0 @@
--- ALE
-vim.g.ale_echo_msg_error_str = 'E'
-vim.g.ale_echo_msg_warning_str = 'W'
-vim.g.ale_echo_msg_format = '[%linter%] %s [%severity%]'
-vim.g.ale_sign_error = "◉"
-vim.g.ale_sign_warning = "◉"
-vim.g.ale_lint_on_enter = 1
-vim.g.ale_lint_on_save = 1
-vim.g.ale_sign_column_always = 1
