b48290d4c371c830e23deb10ac6bb874ef19d80c
Pedro López Mareque <Pedro.lopez.mareque@gmail.com>
2025-05-30 18:28:36 +0200

    feat: update dotfiles

--- .config/nvim/lua/plugins/lsp-zero.lua
+++ .config/nvim/lua/plugins/lsp-zero.lua
@@ -110,6 +110,7 @@ return {
       require('mason-lspconfig').setup({
         ensure_installed = {
           "jsonls",
+          "yamlls",
           "pyright",
           "ruff",
           "ty"
--- .config/nvim/lua/plugins/schema-store.lua
+++ .config/nvim/lua/plugins/schema-store.lua
@@ -1,21 +1,13 @@
 return {
-    "b0o/schemastore.nvim",
-    config = function()
-      require('lspconfig').jsonls.setup {
-        settings = {
-          json = {
-            schemas = require('schemastore').json.schemas(),
-            validate = { enable = true },
-          },
+  "b0o/schemastore.nvim",
+  config = function()
+    require('lspconfig').jsonls.setup {
+      settings = {
+        json = {
+          schemas = require('schemastore').json.schemas(),
+          validate = { enable = true },
         },
-      }
-      require('lspconfig').yamlls.setup {
-        settings = {
-          yaml = {
-            schemas = require('schemastore').yaml.schemas(),
-            validate = { enable = true },
-          },
-        },
-      }
-    end
+      },
+    }
+  end
 }
