fcfc575da336a0e26681b69e6faf5be6811e4bdd
Pedro López Mareque <Pedro.lopez.mareque@gmail.com>
2025-06-01 13:51:50 +0200

    feat: update dotfiles (migrate to blink)

---
+++ .config/nvim/lua/plugins/blink.lua
@@ -0,0 +1,79 @@
+return {
+  { "L3MON4D3/LuaSnip", keys = {} },
+  {
+    "saghen/blink.cmp",
+    dependencies = {
+      "fang2hou/blink-copilot",
+      "rafamadriz/friendly-snippets",
+    },
+    version = "*",
+    config = function()
+      vim.cmd('highlight Pmenu guibg=none')
+      vim.cmd('highlight PmenuExtra guibg=none')
+      vim.cmd('highlight FloatBorder guibg=none')
+      vim.cmd('highlight NormalFloat guibg=none')
+
+      require("blink.cmp").setup({
+        snippets = { preset = "luasnip" },
+        signature = { enabled = true },
+        appearance = {
+          use_nvim_cmp_as_default = false,
+          nerd_font_variant = "normal",
+        },
+        sources = {
+          default = { "copilot", "lsp", "path", "snippets", "buffer" },
+          providers = {
+            cmdline = {
+              min_keyword_length = 2,
+            },
+            copilot = {
+              name = "copilot",
+              module = "blink-copilot",
+              score_offset = 100,
+              async = true,
+            },
+          },
+        },
+        keymap = {
+          preset = 'enter',
+          ['<Tab>'] = { 'select_next', 'fallback' },
+          ['<S-Tab>'] = { 'select_prev', 'fallback' },
+          ["<C-f>"] = {},
+        },
+        cmdline = {
+          enabled = false,
+          completion = { menu = { auto_show = true } },
+          keymap = {
+            ["<CR>"] = { "accept_and_enter", "fallback" },
+          },
+        },
+        completion = {
+          menu = {
+            border = nil,
+            scrolloff = 1,
+            scrollbar = false,
+            draw = {
+              columns = {
+                { "kind_icon" },
+                { "label",      "label_description", gap = 1 },
+                { "kind" },
+                { "source_name" },
+              },
+            },
+          },
+          documentation = {
+            window = {
+              border = nil,
+              scrollbar = false,
+              winhighlight = 'Normal:BlinkCmpDoc,FloatBorder:BlinkCmpDocBorder,EndOfBuffer:BlinkCmpDoc',
+            },
+            auto_show = true,
+            auto_show_delay_ms = 500,
+          },
+        },
+      })
+
+      require("luasnip.loaders.from_vscode").lazy_load()
+    end,
+  },
+}
