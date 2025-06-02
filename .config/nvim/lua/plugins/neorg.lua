e3baf54de8033b54401c55e52ad3aec85c629ccd
Pedro López Mareque <Pedro.lopez.mareque@gmail.com>
2025-06-02 21:12:08 +0200

    feat: update dotfiles

--- .config/nvim/lua/plugins/neorg.lua
+++ .config/nvim/lua/plugins/neorg.lua
@@ -15,6 +15,9 @@ return {
           config = {
             icons = {
               todo = {
+                cancelled = {
+                  icon = "✘",
+                },
                 done = {
                   icon = "✔",
                 }
