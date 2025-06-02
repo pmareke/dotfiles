59207de24b168e01c2c16af98fb5805ca716196a
Pedro López Mareque <Pedro.lopez.mareque@gmail.com>
2025-06-02 08:18:24 +0200

    feat: update dotfiles (migrate to neorg)

--- .config/nvim/lua/plugins/neorg.lua
+++ .config/nvim/lua/plugins/neorg.lua
@@ -1,37 +1,26 @@
 return {
-  {
-    "vhyrro/luarocks.nvim",
-    priority = 1000,
-    config = true,
-  },
-  {
-    "nvim-neorg/neorg",
-    dependencies = { "luarocks.nvim" },
-    version = "*",
-    config = function()
-      require("neorg").setup({
-        load = {
-          ["core.defaults"] = {},
-          ["core.concealer"] = {},
-          ["core.ui.calendar"] = {},
-          ["core.qol.toc"] = {},
-          ["core.qol.todo_items"] = {},
-          ["core.looking-glass"] = {},
-          ["core.presenter"] = { config = { zen_mode = "zen-mode" } },
-          ["core.export"] = {},
-          ["core.export.markdown"] = { config = { extensions = "all" } },
-          ["core.summary"] = {},
-          ["core.tangle"] = { config = { report_on_empty = false } },
-          ["core.dirman"] = {
-            config = {
-              workspaces = {
-                notes = "~/notes",
-              },
-              default_workspace = "notes",
+  "nvim-neorg/neorg",
+  dependencies = { "luarocks.nvim" },
+  lazy = false,
+  version = "*",
+  config = function()
+    require("neorg").setup({
+      load = {
+        ["core.defaults"] = {},
+        ["core.concealer"] = {},
+        ["core.ui.calendar"] = {},
+        ["core.qol.toc"] = {},
+        ["core.qol.todo_items"] = {},
+        ["core.looking-glass"] = {},
+        ["core.dirman"] = {
+          config = {
+            workspaces = {
+              notes = "~/notes",
             },
+            default_workspace = "notes",
           },
         },
-      })
-    end,
-  },
+      },
+    })
+  end,
 }
